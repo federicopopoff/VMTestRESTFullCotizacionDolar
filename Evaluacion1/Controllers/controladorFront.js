f5303acd99bef70d8963326052a3e348ab3fbb4a
Hans Aparicio <hansaparicio@gmail.com>
2017-04-24 19:19:29 -0300

    Inclusion de cliente
    se realizo un cliente que consumia los datos del servicio mediante le metodo $resource

---
+++ Evaluacion1/Controllers/controladorFront.js
@@ -0,0 +1,22 @@
+﻿//(function () {
+
+//creacion del controlador
+    angular
+        .module('app')
+        .controller('myCtrl', myCtrl);
+    //definicion del controlador
+    myCtrl.$inject=['$scope', 'apiservice'];
+
+    function myCtrl($scope, apiservice) {
+
+        //creacion de funcion de obtencion de datos
+        $scope.getInfo = function () {
+            apiservice.query()
+                .$promise
+                    .then(function (response) { console.log(response) })
+                    .catch(function (response) { console.log(response) });
+        };
+    }
+
+
+//})();
---
+++ Evaluacion1/Services/servicioFront.js
@@ -0,0 +1,22 @@
+﻿//(function () {
+
+//creacion del servicio
+angular
+    .module('app')
+    .factory('apiservice', apiservice);
+
+//definicion del servicio
+
+apiservice.$inject = ['$http', '$resource'];
+
+function apiservice($http, $resource) {
+    var resourceUrl = './Cotizacion.svc/moneda';
+    return $resource(
+        resourceUrl,
+        { moneda: 'dolar' },
+        {
+            getInfo: { method: "GET" }
+        });
+}
+
+//})();
