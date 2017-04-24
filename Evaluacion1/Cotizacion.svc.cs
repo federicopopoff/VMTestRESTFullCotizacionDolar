d595b561b12c6996520075791ab7ef3ed25d1873
Hans Aparicio <hansaparicio@gmail.com>
2017-04-24 01:23:12 -0300

    Inicio Proyecto

---
+++ Evaluacion1/Cotizacion.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.Net;
+
+namespace Evaluacion1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Cotizacion" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Cotizacion.svc or Cotizacion.svc.cs at the Solution Explorer and start debugging.
+    public class Cotizacion : ICotizacion
+    {
+        //metodo de comparacion de divisa
+        public bool ComparacionStrings(string moneda, string divisa)
+        {
+            return divisa.Equals(moneda,StringComparison.OrdinalIgnoreCase);
+        }
+        //metodo de conexion con consumo de datos
+        public moneda getData(string sUrlRequest)
+        {
+            var dataBanca = new WebClient().DownloadString(sUrlRequest);
+            string[] data = dataBanca.Split('"');
+            moneda Divisa = new moneda(Convert.ToString( data[1]), Convert.ToString(data[3]), Convert.ToString(data[5]));
+            return Divisa;
+        }
+        //metodo de control de dominio para manipulacion restfull
+        public  moneda getInformationWeb(string tipoMoneda)
+        {
+            if (ComparacionStrings(tipoMoneda,"dolar"))
+            {
+                return getData("https://www.bancoprovincia.com.ar/Principal/Dolar");
+            }
+            else 
+            {
+                if (ComparacionStrings(tipoMoneda, "real") || ComparacionStrings(tipoMoneda, "pesos"))
+                {
+                    throw new WebFaultException(System.Net.HttpStatusCode.Unauthorized);
+                }
+                else
+                {
+                    return new moneda("0", "0", "N/A");
+                }
+            }
+        }
+    }
+    
+}
---
+++ Evaluacion1/moneda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Evaluacion1
+{
+    //Clase para almacenar los valores consumidos desde la web https://www.bancoprovincia.com.ar/Principal/Dolar
+    [DataContract]
+    public class moneda
+    {
+        [DataMember]
+        public string PrecioCompra { get; set; }
+        [DataMember]
+        public string PrecioVenta { get; set; }
+        [DataMember]
+        public string Actualizacion { get; set; }
+
+    //constructor mediante patron factory
+        public moneda(string precioCompra, string precioVenta, string actualizacion)
+        {
+            this.PrecioCompra = precioCompra;
+            this.PrecioVenta = precioVenta;
+            this.Actualizacion = actualizacion;
+        }
+    }
+}
