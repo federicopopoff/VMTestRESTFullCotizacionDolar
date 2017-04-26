96493965b7eb0bd1eadeb90f50c3d9f023a1b51f
Hans Aparicio <hansaparicio@gmail.com>
2017-04-26 00:01:39 -0300

    Inclusion de la caracteristica de tiempo como servicio

--- Evaluacion1/Controllers/controladorFront.js
+++ Evaluacion1/Controllers/controladorFront.js
@@ -4,11 +4,11 @@ angular
     .controller('myCtrl', myCtrl);
 
 //inyeccion
-myCtrl.$inject = ['$scope', 'apiservice']
+myCtrl.$inject = ['$scope', 'apiservice', 'tiempo']
 
 //declaracion de metodos y propiedades
 
-function myCtrl($scope, apiservice) {
+function myCtrl($scope, apiservice, tiempo) {
     $scope.localStorage = localStorage.DataStorage;
 
     if (localStorage.DataStorage != undefined) {
@@ -22,37 +22,8 @@ function myCtrl($scope, apiservice) {
         apiservice.setInfoLocalFromWeb('dolar');
     }
 
-
-
-
-    $scope.haceCuantoTiempo = function (ref) {
-        //obtencion de datos
-        var referencia = ref;
-        var tiempo = new Date();
-        //parse de tiempo
-        var haceSegundos = Math.round((tiempo.getTime() - referencia) / 1000);
-        var haceMinutos = Math.round((tiempo.getTime() - referencia) / 60000);
-        var haceHoras = Math.round((tiempo.getTime() - referencia) / 1440000);
-        //estructura de control
-        if (haceSegundos < 60)
-        {
-            console.log(haceSegundos);
-            return haceSegundos + ' segundos';
-        }
-        else
-        {
-            if (haceMinutos < 60)
-            {
-                console.log(haceMinutos);
-                return haceMinutos + ' minutos';
-            }
-            else
-            {
-                console.log(haceHoras);
-                return haceHoras + ' horas';
-            }
-        }
-        
+    $scope.servicioTiempoAnterior = function (ref) {
+        return tiempo.getTimeAgo(ref);
     }
-    
+
  }
